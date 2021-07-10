11de9fb58adf588c24336c9e2fa9dce46a04f796
Nelo Puchades Bresó <correo@nelo.dev>
2021-07-10 16:53:57 +0200

    Styling - Exercise 1 Extra 1 Finished - Creating Custom Component

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,20 +12,46 @@ import '../box-styles.css';
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
+
+function Box({className, style, children}) {
+  return (
+    <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}}>
+      {children}
+    </div>
+  );
+}
+
+// const smallBox = (
+//   <div className="box box--small" style={{backgroundColor: 'lightblue'}}>
+//     small lightblue box
+//   </div>
+// );
 const smallBox = (
-  <div className="box box--small" style={{backgroundColor: 'lightblue'}}>
+  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
     small lightblue box
-  </div>
+  </Box>
 );
+
+// const mediumBox = (
+//   <div className="box box--medium" style={{backgroundColor: 'pink'}}>
+//     medium pink box
+//   </div>
+// );
 const mediumBox = (
-  <div className="box box--medium" style={{backgroundColor: 'pink'}}>
+  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
     medium pink box
-  </div>
+  </Box>
 );
+
+// const largeBox = (
+//   <div className="box box--large" style={{backgroundColor: 'orange'}}>
+//     large orange box
+//   </div>
+// );
 const largeBox = (
-  <div className="box box--large" style={{backgroundColor: 'orange'}}>
+  <Box className="box--large" style={{backgroundColor: 'orange'}}>
     large orange box
-  </div>
+  </Box>
 );
 
 function App() {
